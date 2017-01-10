b1d214bad2374b17666fbe095831fded1d9a8f6b
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-01-10 10:30:19 -0200

    Better file organization

---
+++ src/log.js
@@ -0,0 +1,23 @@
+const _logMsg = (scope, tag, msg) => {
+  return `[${scope}] [${tag}] ${msg}`;
+};
+
+const Log = {
+  v: (tag, msg) => {
+    console.log(_logMsg('verbose', tag, msg));
+  },
+  d: (tag, msg) => {
+    console.debug(_logMsg('debug', tag, msg));
+  },
+  i: (tag, msg) => {
+    console.info(_logMsg('info', tag, msg));
+  },
+  w: (tag, msg) => {
+    console.warn(_logMsg('warning', tag, msg));
+  },
+  e: (tag, msg) => {
+    console.error(_logMsg('error', tag, msg));
+  }
+};
+
+module.exports = Log;
--- src/main.js
+++ src/main.js
@@ -1,27 +1,7 @@
-const _logMsg = (scope, tag, msg) => {
-  return `[${scope}] [${tag}] ${msg}`;
-};
-
-const Log = {
-  v: (tag, msg) => {
-    console.log(_logMsg('verbose', tag, msg));
-  },
-  d: (tag, msg) => {
-    console.debug(_logMsg('debug', tag, msg));
-  },
-  i: (tag, msg) => {
-    console.info(_logMsg('info', tag, msg));
-  },
-  w: (tag, msg) => {
-    console.warn(_logMsg('warning', tag, msg));
-  },
-  e: (tag, msg) => {
-    console.error(_logMsg('error', tag, msg));
-  }
-};
+import Log from './log';
 
 if (typeof window !== 'undefined') {
   window.Log = Log;
 }
 
-export default Log;
+module.exports = Log;
